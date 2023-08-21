60d87f9b4f14313e69e4ad925bf7666a1fbf8a4a
brunosllz <brunoslvrlz13@gmail.com>
2023-08-21 19:55:33 -0300

    feat: add pino how logger provider at application

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
+import { LoggerService } from '@common/infra/logger/logger.service';
 import { NestFactory } from '@nestjs/core';
+import { LoggerErrorInterceptor } from 'nestjs-pino';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const LoggerServiceInstance = app.get(LoggerService);
+
+  app.useLogger(LoggerServiceInstance);
+  app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
   await app.listen(3000);
 }
 bootstrap();
