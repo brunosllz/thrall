b72c47dd2d63c2346b2fb04cafc18bb744836805
brunosllz <brunoslvrlz13@gmail.com>
2023-08-23 17:17:10 -0300

    feat: add factory to create entity in database

--- test/factories/make-answer.ts
+++ test/factories/make-answer.ts
@@ -1,10 +1,12 @@
+import { PrismaService } from '@common/infra/prisma/prisma.service';
 import { faker } from '@faker-js/faker';
 import { Answer, AnswerProps } from '@modules/timeline/domain/entities/answer';
+import { AnswerMapper } from '@modules/timeline/infra/prisma/mappers/answer-mapper';
+import { Injectable } from '@nestjs/common';
 
-export function makeFakeAnswer(
-  override: Partial<AnswerProps> = {},
-  id?: string,
-) {
+type Overrides = Partial<AnswerProps>;
+
+export function makeFakeAnswer(override: Overrides = {}, id?: string) {
   const answer = Answer.create(
     {
       authorId: faker.string.uuid(),
@@ -16,4 +18,19 @@ export function makeFakeAnswer(
   );
 
   return answer;
+}
+
+@Injectable()
+export class AnswerFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makeAnswer(data = {} as Overrides) {
+    const answer = makeFakeAnswer(data);
+
+    await this.prisma.answer.create({
+      data: AnswerMapper.toPersistence(answer),
+    });
+
+    return answer;
+  }
 }
