defe76941d752825ba992dde0d4f2d0594051b4b
brunosllz <brunoslvrlz13@gmail.com>
2023-08-28 01:30:07 -0300

    chore: adjust types

--- src/modules/timeline/infra/prisma/mappers/role-mapper.ts
+++ src/modules/timeline/infra/prisma/mappers/role-mapper.ts
@@ -2,13 +2,18 @@ import { Role } from '@modules/timeline/domain/entities/role';
 import { Slug } from '@modules/timeline/domain/entities/value-objects/slug';
 import { Role as RawRole } from '@prisma/client';
 
-type ToDomainRawProps = { projectId: string; amount: number; role: RawRole };
+type ToDomainRawProps = {
+  projectId: string;
+  membersAmount: number;
+  role: RawRole;
+};
 
 export class RoleMapper {
   static toDomain(raw: ToDomainRawProps): Role {
     const role = Role.create(
       {
-        membersAmount: raw.amount,
+        projectId: raw.projectId,
+        membersAmount: raw.membersAmount,
         name: Slug.createFromText(raw.role.name).getValue(),
       },
       raw.role.id,
