6c90db23bffcdfee9f50e0509380472ec1f6d165
brunosllz <brunoslvrlz13@gmail.com>
2023-09-21 01:27:20 -0300

    test: refactor and add new test
    refactor answer and project e2e tests and add get project from user e2e test

--- src/modules/project-management/infra/http/controllers/answer.controller.e2e-spec.ts
+++ src/modules/project-management/infra/http/controllers/answer.controller.e2e-spec.ts
@@ -1,31 +1,29 @@
-import { PrismaDatabaseModule as PrismaDatabaseAccountModule } from '@modules/account/infra/prisma/prisma-database.module';
+import { AppModule } from '@/app.module';
+import { PrismaService } from '@/common/infra/prisma/prisma.service';
 import { INestApplication } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import request from 'supertest';
 
 import { ProjectFactory } from '@test/factories/make-project';
 import { UserFactory } from '@test/factories/make-user';
 
-import { PrismaDatabaseModule as PrismaDatabaseTImeLineModule } from '../../prisma/prisma-database.module';
-import { CreateAnswerDTO } from '../validation-schemas/create-answer-dto';
-import { HttpModule } from '../http.module';
+import { CreateAnswerBodySchema } from '../validation-schemas/create-answer-schema';
 
 describe('AnswerController (e2e)', () => {
   let app: INestApplication;
   let userFactory: UserFactory;
   let projectFactory: ProjectFactory;
+  let jwt: JwtService;
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
-      imports: [
-        PrismaDatabaseTImeLineModule,
-        PrismaDatabaseAccountModule,
-        HttpModule,
-      ],
-      providers: [UserFactory, ProjectFactory],
+      imports: [AppModule],
+      providers: [UserFactory, ProjectFactory, PrismaService],
     }).compile();
 
     app = moduleRef.createNestApplication();
+    jwt = moduleRef.get(JwtService);
 
     projectFactory = moduleRef.get(ProjectFactory);
     userFactory = moduleRef.get(UserFactory);
@@ -34,22 +32,23 @@ describe('AnswerController (e2e)', () => {
   });
 
   it('/answer (POST) - create a answer', async () => {
-    const mainUser = await userFactory.makeUser();
+    const userProjectOwner = await userFactory.makeUser();
 
     const project = await projectFactory.makeProject({
-      authorId: mainUser.id,
+      authorId: userProjectOwner.id,
     });
 
     const user = await userFactory.makeUser();
+    const accessToken = jwt.sign({ uid: user.id });
 
-    const createAnswerBody: CreateAnswerDTO = {
-      authorId: user.id,
+    const createAnswerBody: CreateAnswerBodySchema = {
       content: 'content example',
       projectId: project.id,
     };
 
     await request(app.getHttpServer())
       .post('/answers')
+      .set('Authorization', `Bearer ${accessToken}`)
       .send(createAnswerBody)
       .expect(201);
   });
