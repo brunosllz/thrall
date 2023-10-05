12a69fdedb718f30517e4d09e4b3da531d373ab7
brunosllz <brunoslvrlz13@gmail.com>
2023-10-04 21:11:10 -0300

    test: alter test runner - change jest to vitest

--- src/common/infra/config/env/index.ts
+++ src/common/infra/config/env/index.ts
@@ -8,6 +8,9 @@ export const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   DATABASE_URL: z.string().url(),
   JWT_SECRET_KEY: z.string(),
+  REDIS_HOST: z.string().optional().default('localhost'),
+  REDIS_PORT: z.coerce.number().optional().default(6379),
+  REDIS_DB: z.coerce.number().optional().default(0),
 });
 
 export type Env = z.infer<typeof envSchema>;
